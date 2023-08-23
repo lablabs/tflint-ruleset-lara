ee355b45ca5d3bd35a4cf331f4f3026cf5ebd0d6
Michal Muransky <michal.muransky@lablabs.io>
2023-08-23 16:46:12 +0200

    wip

--- Makefile
+++ Makefile
@@ -8,4 +8,4 @@ build:
 
 install: build
 	mkdir -p ~/.tflint.d/plugins
-	mv ./tflint-ruleset-template ~/.tflint.d/plugins
+	mv ./tflint-ruleset-whitelisted-module-sources ~/.tflint.d/plugins
--- main.go
+++ main.go
@@ -3,19 +3,16 @@ package main
 import (
 	"github.com/terraform-linters/tflint-plugin-sdk/plugin"
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
-	"github.com/terraform-linters/tflint-ruleset-template/rules"
+	"github.com/lablabs/tflint-ruleset-whitelisted-module-sources/rules"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		RuleSet: &tflint.BuiltinRuleSet{
-			Name:    "template",
+			Name:    "whitelisted-module-sources",
 			Version: "0.1.0",
 			Rules: []tflint.Rule{
-				rules.NewAwsInstanceExampleTypeRule(),
-				rules.NewAwsS3BucketExampleLifecycleRule(),
-				rules.NewGoogleComputeSSLPolicyRule(),
-				rules.NewTerraformBackendTypeRule(),
+				rules.NewTerraformModuleWhitelistedSourcesRule(),
 			},
 		},
 	})
