aa34e632ea703ce20426957604c6a647cdf03c42
Martin Hauskrecht <hauskrechtmartin@gmail.com>
2023-08-23 20:24:22 +0200

    Working 1

--- rules/terraform_module_whitelisted_sources.go
+++ rules/terraform_module_whitelisted_sources.go
@@ -2,13 +2,12 @@ package rules
 
 import (
 	"fmt"
-	"path/filepath"
-	"golang.org/x/exp/slices"
 
-	"github.com/hashicorp/go-getter"
+	"github.com/terraform-linters/tflint-plugin-sdk/hclext"
+	"github.com/terraform-linters/tflint-plugin-sdk/logger"
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
+
 	"github.com/terraform-linters/tflint-ruleset-terraform/terraform"
-	"github.com/terraform-linters/tflint-ruleset-terraform/project"
 )
 
 type TerraformModuleWhitelistedSourcesRule struct {
@@ -16,15 +15,15 @@ type TerraformModuleWhitelistedSourcesRule struct {
 	attributeName string
 }
 
-type TerraformModuleWhitelistedSourcesRuleConfig struct {
-	Whitelist []string `hclext:"whitelist,optional"`
-}
-
 // NewTerraformModuleWhitelistedSourcesRule returns a new rule
 func NewTerraformModuleWhitelistedSourcesRule() *TerraformModuleWhitelistedSourcesRule {
 	return &TerraformModuleWhitelistedSourcesRule{
 		attributeName: "source",
 	}
+}
+
+type TerraformModuleWhitelistedSourcesRuleConfig struct {
+	Whitelist []string `hclext:"whitelist,optional"`
 }
 
 // Name returns the rule name
@@ -38,69 +37,87 @@ func (r *TerraformModuleWhitelistedSourcesRule) Enabled() bool {
 }
 
 func (r *TerraformModuleWhitelistedSourcesRule) Severity() tflint.Severity {
-	return tflint.WARNING
+	return tflint.ERROR
 }
 
 func (r *TerraformModuleWhitelistedSourcesRule) Link() string {
-	return project.ReferenceLink(r.Name())
+	// return project.ReferenceLink(r.Name())
+	return ""
 }
 
 // Check checks whether module source is whitelisted
-func (r *TerraformModuleWhitelistedSourcesRule) Check(rr tflint.Runner) error {
-	runner := rr.(*terraform.Runner)
-
-	path, err := runner.GetModulePath()
+func (r *TerraformModuleWhitelistedSourcesRule) Check(runner tflint.Runner) error {
+	resources, err := runner.GetResourceContent("aws_route53_record", &hclext.BodySchema{
+		Attributes: []hclext.AttributeSchema{
+			{Name: "source"},
+		},
+	}, nil)
 	if err != nil {
 		return err
 	}
-	if !path.IsRoot() {
-		// This rule does not evaluate child modules.
-		return nil
-	}
-	
-	config := TerraformModuleWhitelistedSourcesRuleConfig{}
-	config.Whitelist = append(config.Whitelist, "git::https://github.com/lablabs/")
-	if err := runner.DecodeRuleConfig(r.Name(), &config); err != nil {
-		return err
-	}
 
-	calls, diags := runner.GetModuleCalls()
-	if diags.HasErrors() {
-		return diags
-	}
+	// Put a log that can be output with `TFLINT_LOG=debug`
+	logger.Error(fmt.Sprintf("Get %d instances", len(resources.Blocks)))
 
-	for _, call := range calls {
-		if err := r.checkModule(runner, call, config); err != nil {
-			return err
+	for _, resource := range resources.Blocks {
+		attribute, exists := resource.Body.Attributes["source"]
+		if !exists {
+			logger.Info("Attribute does not exist")
+			continue
 		}
-	}
 
+		// err := runner.EvaluateExpr(attribute.Expr, func(instanceType string) error {
+		// 	return runner.EmitIssue(
+		// 		r,
+		// 		fmt.Sprintf("instance type is %s", instanceType),
+		// 		attribute.Expr.Range(),
+		// 	)
+		// }, nil)
+		// if err != nil {
+		// 	return err
+		// }
+		return runner.EmitIssue(
+			r,
+			fmt.Sprintf("instance type is %s", "test"),
+			attribute.Expr.Range(),
+		)
+	}
 	return nil
 }
 
 func (r *TerraformModuleWhitelistedSourcesRule) checkModule(runner tflint.Runner, module *terraform.ModuleCall, config TerraformModuleWhitelistedSourcesRuleConfig) error {
-	source, err := getter.Detect(module.Source, filepath.Dir(module.DefRange.Filename), []getter.Detector{
-		// https://github.com/hashicorp/terraform/blob/51b0aee36cc2145f45f5b04051a01eb6eb7be8bf/internal/getmodules/getter.go#L30-L52
-		new(getter.GitHubDetector),
-		new(getter.GitDetector),
-		new(getter.BitBucketDetector),
-		new(getter.GCSDetector),
-		new(getter.S3Detector),
-		new(getter.FileDetector),
-	})
-	if err != nil {
-		return err
-	}
 
+	logger.Info(fmt.Sprintf("Source: %v", module.Source))
 
-	if !slices.Contains(config.Whitelist, source) {
-		return runner.EmitIssue(
-			r,
-			fmt.Sprintf("Module source %s is not whitelisted", source),
-			module.DefRange,
-		)
-	}
-
-	return nil
-
+	return runner.EmitIssue(
+		r,
+		fmt.Sprintf(`Module source "%s" is not pinned`, module.Source),
+		module.SourceAttr.Expr.Range(),
+	)
 }
+
+// func (r *TerraformModuleWhitelistedSourcesRule) checkModule(runner tflint.Runner, module *terraform.ModuleCall, config TerraformModuleWhitelistedSourcesRuleConfig) error {
+// 	source, err := getter.Detect(module.Source, filepath.Dir(module.DefRange.Filename), []getter.Detector{
+// 		// https://github.com/hashicorp/terraform/blob/51b0aee36cc2145f45f5b04051a01eb6eb7be8bf/internal/getmodules/getter.go#L30-L52
+// 		new(getter.GitHubDetector),
+// 		new(getter.GitDetector),
+// 		new(getter.BitBucketDetector),
+// 		new(getter.GCSDetector),
+// 		new(getter.S3Detector),
+// 		new(getter.FileDetector),
+// 	})
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	if !slices.Contains(config.Whitelist, source) {
+// 		return runner.EmitIssue(
+// 			r,
+// 			fmt.Sprintf("Module source %s is not whitelisted", source),
+// 			module.DefRange,
+// 		)
+// 	}
+
+// 	return nil
+
+// }
